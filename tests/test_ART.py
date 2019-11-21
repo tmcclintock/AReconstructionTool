b9edcf7e88c0ca4cbfb2872ece34109883845a66
Tom McClintock <tmcclintock89@gmail.com>
2019-11-21 09:56:16 -0700

    Making the first test.

---
+++ tests/test_ART.py
@@ -0,0 +1,10 @@
+"""Tests of the reconstruction tool
+"""
+import numpy as np
+import scipy.stats as ss
+import numpy.testing as npt
+import pytest
+
+def test_ART_basics():
+    #Smoke tests
+    pass
